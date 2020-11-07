50445257282c5b2dfcd6c0139f317870d624f3af
blaisedev <blaisedevaney@gmail.com>
2020-11-07 17:40:21 +0000

    Updates

---
+++ precache-manifest.53cba2bb33dc9729c353ab2fbe2464b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8ab360160a2dfdc963895ec4ace076",
+    "url": "/aidez-moi/index.html"
+  },
+  {
+    "revision": "74c02d6d44bc17d212ed",
+    "url": "/aidez-moi/static/css/main.366cc804.chunk.css"
+  },
+  {
+    "revision": "bda15da9bf40e980c714",
+    "url": "/aidez-moi/static/js/2.30e3991e.chunk.js"
+  },
+  {
+    "revision": "af658794ed724b08ebee181ec2d12561",
+    "url": "/aidez-moi/static/js/2.30e3991e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c02d6d44bc17d212ed",
+    "url": "/aidez-moi/static/js/main.ad3c83a7.chunk.js"
+  },
+  {
+    "revision": "230de4120a564ee12017",
+    "url": "/aidez-moi/static/js/runtime-main.aa3f9d81.js"
+  }
+]);
